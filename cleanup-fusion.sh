202db8c662576011532cea3c9210c3e783b3824a
Jackie Ng <jumpinjackie@gmail.com>
2019-02-03 21:20:11 +1100

    Add default .gitignore for fusion

--- cleanup-fusion.sh
+++ cleanup-fusion.sh
@@ -6,4 +6,6 @@ pushd prepared/fusion.git
 git filter-branch --msg-filter "perl -lape 's/#(\d+)/\[https:\/\/trac\.osgeo\.org\/fusion\/ticket\/\$1\]/g'" -- --all
 git reflog expire --expire=now --all && git gc --prune=now --aggressive
 git remote add origin https://github.com/jumpinjackie/fusion.git
+# Add extras
+cp ../../overlays/fusion/.gitignore .
 popd
