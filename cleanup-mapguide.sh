6be4a8df96c5079438646ae9fec2b8a60876c104
jumpinjackie <jumpinjackie@gmail.com>
2019-07-28 06:26:09 +0000

    Remove dbxml docs. We're using regular git filter-branch to do this as bfg doesn't support directories. This extends execution time of this script by several hours, but as this script is not meant to be run very frequently, it's not a major issue. Fixes #1

--- cleanup-mapguide.sh
+++ cleanup-mapguide.sh
@@ -5,6 +5,11 @@ cp -R pristine/mapguide.git prepared/mapguide.git
 pushd prepared/mapguide.git
 java -jar ../../bfg.jar --protect-blobs-from master,3.1 --delete-folders Maestro --strip-blobs-bigger-than 5M --private
 git filter-branch --msg-filter "perl -lape 's/#(\d+)/\[https:\/\/trac\.osgeo\.org\/mapguide\/ticket\/\$1\]/g'" -- --all
+# There is a whole bunch of dbxml docs we don't care to preserve
+git filter-branch -f --tree-filter 'rm -rf MgDev/Oem/dbxml/db-4.8.26/docs' HEAD
+git filter-branch -f --tree-filter 'rm -rf MgDev/Oem/dbxml/dbxml/docs' HEAD
+git filter-branch -f --tree-filter 'rm -rf MgDev/Oem/dbxml/xqilla/docs' HEAD
+git filter-branch -f --tree-filter 'rm -rf MgDev/Oem/dbxml/xerces-c-src/doc' HEAD
 git reflog expire --expire=now --all && git gc --prune=now --aggressive
 # Prune Maestro Branches
 git branch | grep [Mm]aestro | xargs -L1 git branch -D
