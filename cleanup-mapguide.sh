f29c4725d888dc96382beb4df5750ae48262b718
Jackie Ng <jumpinjackie@gmail.com>
2019-07-31 22:40:32 +1000

    Try getting BFG to clean unwanted folders and raise the blob stripping size limit as the old limit nukes legitimate test data files

--- cleanup-mapguide.sh
+++ cleanup-mapguide.sh
@@ -3,14 +3,14 @@
 rm -rf prepared/mapguide.git
 cp -R pristine/mapguide.git prepared/mapguide.git
 pushd prepared/mapguide.git
-java -jar ../../bfg.jar --protect-blobs-from master,3.1 --delete-folders Maestro --strip-blobs-bigger-than 5M --private
+java -jar ../../bfg.jar --protect-blobs-from master,3.1 --delete-folders {Maestro,MgDev/Oem/dbxml/db-4.8.26/docs,MgDev/Oem/dbxml/dbxml/docs,MgDev/Oem/dbxml/xqilla/docs,MgDev/Oem/dbxml/xerces-c-src/doc} --strip-blobs-bigger-than 12M --private
 git filter-branch --msg-filter "perl -lape 's/#(\d+)/\[https:\/\/trac\.osgeo\.org\/mapguide\/ticket\/\$1\]/g'" -- --all
 # There is a whole bunch of dbxml docs we don't care to preserve
-git filter-branch -f --tree-filter 'rm -rf MgDev/Oem/dbxml/db-4.8.26/docs' HEAD
-git filter-branch -f --tree-filter 'rm -rf MgDev/Oem/dbxml/dbxml/docs' HEAD
-git filter-branch -f --tree-filter 'rm -rf MgDev/Oem/dbxml/xqilla/docs' HEAD
-git filter-branch -f --tree-filter 'rm -rf MgDev/Oem/dbxml/xerces-c-src/doc' HEAD
-git reflog expire --expire=now --all && git gc --prune=now --aggressive
+#git filter-branch -f --tree-filter 'rm -rf MgDev/Oem/dbxml/db-4.8.26/docs' HEAD
+#git filter-branch -f --tree-filter 'rm -rf MgDev/Oem/dbxml/dbxml/docs' HEAD
+#git filter-branch -f --tree-filter 'rm -rf MgDev/Oem/dbxml/xqilla/docs' HEAD
+#git filter-branch -f --tree-filter 'rm -rf MgDev/Oem/dbxml/xerces-c-src/doc' HEAD
+#git reflog expire --expire=now --all && git gc --prune=now --aggressive
 # Prune Maestro Branches
 git branch | grep [Mm]aestro | xargs -L1 git branch -D
 git tag -l | grep MAESTRO | xargs -L1 git tag -d
